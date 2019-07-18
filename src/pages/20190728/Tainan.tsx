ab9336dedf5fccad7196f17604c3345452313cf0
bcjohn <bcjohnblue@gmail.com>
2019-07-18 17:54:11 +0800

    wip

--- src/pages/20190728/Tainan.tsx
+++ src/pages/20190728/Tainan.tsx
@@ -8,25 +8,8 @@ const Tainan20190728: React.FC = () => {
     const { name, articles } = type;
     return <ArticleType name={name} articles={articles} key={index} />;
   });
-  // const checkInDOM = checkIn.map((item, index) => {
-  //   const { title, img, intro } = item;
-  //   return <Article title={title} img={img} intro={intro} key={index} />;
-  // });
-  // const dessertDOM = dessert.map((item, index) => {
-  //   const { title, img, intro } = item;
-  //   return <Article title={title} img={img} intro={intro} key={index} />;
-  // });
 
-  return (
-    <div className={styles.tainan}>
-      {articleDOM}
-      {/* <ArticleType></ArticleType>
-      <div className={styles.type_title}>[台南] IG 打卡景點</div>
-      {checkInDOM}
-      <div className={styles.type_title}>[台南] 甜點、下午茶</div>
-      {dessertDOM} */}
-    </div>
-  );
+  return <div className={styles.tainan}>{articleDOM}</div>;
 };
 
 export default Tainan20190728;
