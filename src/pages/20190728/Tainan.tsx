14348ad9e5c5636bd3f9bd95cbe7f9fda82fc144
bcjohn <bcjohnblue@gmail.com>
2019-07-18 11:58:48 +0800

    add tslint

---
+++ src/pages/20190728/Tainan.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Tainan.module.sass';
+
+const Tainan20190728: React.FC = () => {
+  return <div className={styles.app}>123</div>;
+};
+
+export default Tainan20190728;
