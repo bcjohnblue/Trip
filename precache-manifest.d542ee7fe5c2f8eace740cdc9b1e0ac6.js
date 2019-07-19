df26c0c5c61768c7e3a926e44df781127db00663
bcjohn <bcjohnblue@gmail.com>
2019-07-19 17:42:40 +0800

    Updates

---
+++ precache-manifest.d542ee7fe5c2f8eace740cdc9b1e0ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc3888ce11116bb495aa4ba64305e64",
+    "url": "/trip/index.html"
+  },
+  {
+    "revision": "636619d02bf93e58c838",
+    "url": "/trip/static/css/main.7ce7e7e5.chunk.css"
+  },
+  {
+    "revision": "9b98313426ed8b2e5ae9",
+    "url": "/trip/static/js/2.91aa2fc5.chunk.js"
+  },
+  {
+    "revision": "636619d02bf93e58c838",
+    "url": "/trip/static/js/main.27acafa4.chunk.js"
+  },
+  {
+    "revision": "d650eb8a31c56b8cca00",
+    "url": "/trip/static/js/runtime~main.b1ed6f75.js"
+  }
+]);
